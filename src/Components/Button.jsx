8cbc727837a297ecc00e59bcf2546132d04abd62
Thesy Nyembo <nyembothesy@gmail.com>
2020-08-18 20:38:20 +0200

    mon commit

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import { Button } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 
-
-
 const ButtonExampleInverted = () => (
   <div>
     <Link to="/">
